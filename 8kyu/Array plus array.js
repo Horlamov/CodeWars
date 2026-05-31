92525aa1bb0e4e0fc90791c60633ddc7c9a6a085
Denis H <XoMA32@yandex.ru>
2026-05-31 22:25:40 +0300

    Array plus array

---
+++ 8kyu/Array plus array.js
@@ -0,0 +1,23 @@
+// ❓DESCRIPTION
+
+// I'm new to coding and now I want to get the sum of two arrays... Actually the sum of all their elements. I'll appreciate for your help.
+//
+//     P.S. Each array includes only integer numbers. Output is a number too.
+
+
+// 🇷🇺 РУС
+
+// Я новичок в программировании, и теперь мне нужно получить сумму двух массивов... Точнее, сумму всех их элементов. Буду благодарен за помощь.
+//
+//     P.S. Каждый массив содержит только целые числа. Результатом также является число.
+
+
+//  ✅ SOLUTION
+
+function arrayPlusArray(arr1, arr2) {
+
+
+
+    return arr1 + arr2;
+}
+
