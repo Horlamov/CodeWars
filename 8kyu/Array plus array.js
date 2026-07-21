2944b4e7066a3b55551d864621ac6ce59a4272a8
Denis H <XoMA32@yandex.ru>
2026-07-21 22:42:50 +0300

    Array plus array. solution

--- 8kyu/Array plus array.js
+++ 8kyu/Array plus array.js
@@ -14,19 +14,10 @@
 
 //  ✅ SOLUTION
 function arrayPlusArray(arr1, arr2) {
-    let a = 0;
-
-    for (let i = 0; i < arr1.length; i++) {
-
-         a = a + arr1[i];
-    }
-
-    for (let i = 0; i < arr2.length; i++) {
-
-        a = a + arr2[i];
-
-    }
-    return a;
+    let a = [];
+    a = arr1.concat(arr2);
+    console.log(a);
+    return a.reduce((a, b) => a + b)
 }
 
 console.log(arrayPlusArray([1, 2, 3],[4,5,6]));
